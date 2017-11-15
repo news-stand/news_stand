af10c6e3d2ddc86432b1fcdf33df4ed6d5ebe4dd
Ayush Kumar <ayushkumar26@gmail.com>
2017-11-15 15:28:40 -0800

    click handlers for toggle view  and refresh firing

--- app/components/App.js
+++ app/components/App.js
@@ -4,12 +4,24 @@ import Header from './Header'
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.onRefreshClick = this.onRefreshClick.bind(this);
+    this.onToggleClick = this.onToggleClick.bind(this);
   }
+
+  onRefreshClick() {
+    // trigger get request to server
+  }
+
+  onToggleClick() {
+    // invert article-view state
+  }
+
   render() {
     return (
       <div>
         Hello World!
-        <Header />
+        <Header onRefreshClick={this.onRefreshClick} onToggleClick={this.onToggleClick} />
       </div>
     );
   }
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 
-const Header = () => (
+const Header = (props) => (
   <div>
     <form>
       [News Stand Logo]
-      <button type="button">Toggle</button>
-      <button type="button">Refresh</button>
+      <button type="button" onClick={props.onToggleClick}>Toggle</button>
+      <button type="button" onClick={props.onRefreshClick}>Refresh</button>
     </form>
   </div>
 );
