28d899b32831f8ba7c0094cfea7338e65a7b1828
Katherine Manning <katm201@gmail.com>
2017-11-15 13:10:47 -0800

    Copy over files from original repo

---
+++ app/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>Hello World!</div>
+    );
+  }
+}
+
+export default App;
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './public/assets/styles.css';
+
+import App from './components/App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
