518785694c98b27050715fe233f7192a292f2fb0
Katherine Manning <katm201@gmail.com>
2017-11-15 13:42:56 -0800

    Add dummy data for sources and articles

---
+++ app/dummy-data/articles.js
@@ -0,0 +1,89 @@
+const dummyArticles = {
+    "status": "ok",
+    "source": "the-next-web",
+    "sortBy": "latest",
+    "articles": [
+        {
+            "author": "Tristan Greene",
+            "title": "Google brings on-device machine learning to mobile with TensorFlow Lite",
+            "description": "Google's recently released developer's preview of TensorFlow Lite is a machine learning platform for mobile that brings the power of algorithms to phones.",
+            "url": "https://thenextweb.com/artificial-intelligence/2017/11/15/google-brings-on-device-machine-learning-to-mobile-with-tensorflow-lite/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/tensorflow-social.jpg",
+            "publishedAt": "2017-11-15T21:18:05Z"
+        },
+        {
+            "author": "Inés Casserly",
+            "title": "Google tidies up its Maps with new icons and color keys",
+            "description": "Google today announced it’s bringing a new look to Maps. The new experience has brighter colors, clearer icons and an overall improved user experience. Over the next few weeks users will be introduced to new color keys and icons depending on the category, such as orange for food and drink and pink for health. This …",
+            "url": "https://thenextweb.com/google/2017/11/15/google-tidies-up-its-maps-with-new-icons-and-color-keys/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/Screen-Shot-2017-11-15-at-5.13.11-PM-social.png",
+            "publishedAt": "2017-11-15T21:00:49Z"
+        },
+        {
+            "author": "Inés Casserly",
+            "title": "Bose is crowdfunding noise-cancelling earbuds that can help you sleep",
+            "description": "Could this be the solution to a good night's sleep?",
+            "url": "https://thenextweb.com/insider/2017/11/15/bose-is-crowdfunding-noise-cancelling-earbuds-that-can-help-you-sleep/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/Screen-Shot-2017-11-15-at-4.12.48-PM-social.png",
+            "publishedAt": "2017-11-15T20:01:50Z"
+        },
+        {
+            "author": "Napier Lopez",
+            "title": "Moment's iPhone X cases turn it into an interchangeable lens camera",
+            "description": "Even though smartphones have gotten good enough to compete with DSLR or mirrorless cameras in certain everyday scenarios, they still have one big disadvantage: fixed focal lengths. Despite smartphones now occasionally sporting two lenses, they don’t quite have the flexibility of a real interchangeable lens camera. That’s where accessories like Moment’s smartphone lenses come in. …",
+            "url": "https://thenextweb.com/apple/2017/11/15/moments-iphone-x-cases-turn-interchangeable-lens-camera/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/Moment-iPhone-10-case-2-social.jpg",
+            "publishedAt": "2017-11-15T19:44:04Z"
+        },
+        {
+            "author": "Rachel Kaser",
+            "title": "Skype fined $36k for not providing data to Belgian authorities",
+            "description": "Skype lost its appeal with a Belgian court which fined the company for not handing over messages -- which Skype says would have been effectively impossible.",
+            "url": "https://thenextweb.com/microsoft/2017/11/15/skype-fined-36k-belgian-authorities/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2014/02/skype_android_2-social.jpg",
+            "publishedAt": "2017-11-15T18:33:27Z"
+        },
+        {
+            "author": "Tristan Greene",
+            "title": "Blochchainify freedom by mining cryptocurrency for bail money",
+            "description": "Here's how you can donate your unused processing power to help get impoverished people a shot at a fair trial by mining cryptocurrency for bail money.",
+            "url": "https://thenextweb.com/cryptocurrency/2017/11/15/help-blockchainify-people-out-of-jail-by-mining-cryptocurrency-for-bail/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/jail-social.jpg",
+            "publishedAt": "2017-11-15T18:21:25Z"
+        },
+        {
+            "author": "Mix",
+            "title": "Bitcoin surges past the $7,000 barrier again",
+            "description": "Shortly after Bitcoin crossed the $7,500 mark to hit an all-time high almost two weeks ago, the leading cryptocurrency has surged past $7,000 yet again.",
+            "url": "https://thenextweb.com/hardfork/2017/11/15/bitcoin-price-7000-usd/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/bitcoin-1-1.jpg",
+            "publishedAt": "2017-11-15T16:58:44Z"
+        },
+        {
+            "author": "Steve Olenski",
+            "title": "5 tips to help successfully market an ICO",
+            "description": "ICOs, or initial coin offerings, are becoming increasingly popular, creating a multi-billion-dollar market. There is no sign of this industry slowing down any time soon, as ICOs are ...",
+            "url": "http://data.thenextweb.com/tnw/post/5_tips_to_help_successfully_market_an_ico",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/initial_coin_offering-social.jpeg",
+            "publishedAt": "2017-11-15T16:56:00Z"
+        },
+        {
+            "author": "Camille Charluet",
+            "title": "This AR mural is the perfect way to celebrate Australia's ‘YES’ vote for same-sex marriage",
+            "description": "Marriage equality might finally become a reality in Australia by Christmas and this satirical AR mural helps us celebrate.",
+            "url": "https://thenextweb.com/augmented-reality/2017/11/15/ar-celebrate-australias-for-same-sex-marriage/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/Screen-Shot-2017-11-15-at-17.30.30-social.png",
+            "publishedAt": "2017-11-15T16:53:25Z"
+        },
+        {
+            "author": "Matthew Hughes",
+            "title": "This Twitter bot will colorize your black-and-white photos in seconds for free",
+            "description": "Colorise Bot is scary accurate, and super fast.",
+            "url": "https://thenextweb.com/artificial-intelligence/2017/11/15/this-twitter-bot-will-colorize-your-black-and-white-photos-in-seconds-for-free/",
+            "urlToImage": "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2017/11/black-and-white-city-houses-skyline-social.jpg",
+            "publishedAt": "2017-11-15T16:53:02Z"
+        }
+    ]
+};
+
+export default dummyArticles;
