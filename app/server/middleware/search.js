5fbc989659533d9753b71f5766165d5223e11604
Katherine Manning <katm201@gmail.com>
2017-11-15 19:20:53 -0800

    Add comments to explain limitations of current API hookup

--- app/server/middleware/search.js
+++ app/server/middleware/search.js
@@ -4,7 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const searchArticles = (request, response, next) => {
-  // starting with one source at a time, no sortBy
+  // only searches a single source, rather than an
+  // array of sources
+  // also doesn't allow sortBy filtering yet, as some
+  // sources don't support all types of filtering
   const { source } = request.query;
 
   const search = `https://newsapi.org/v1/articles?apiKey=${process.env.NEWS_KEY}&source=${source}`;
