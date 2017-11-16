b19dbc4fd547a2cb9ed44d97d8fa0d93c75441f4
Katherine Manning <katm201@gmail.com>
2017-11-16 14:34:24 -0800

    (refactor) fix searchArticles to use a helper function for the axios call

--- app/server/middleware/search.js
+++ app/server/middleware/search.js
@@ -3,30 +3,36 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const getArticles = (baseSearch, sortBy, successCallback, failureCallback) => {
+  axios.get(`${baseSearch}&sortBy=${sortBy}`)
+    .then((newsResponse) => {
+      successCallback(newsResponse.data);
+    })
+    .catch((err) => {
+      failureCallback(err);
+    });
+};
+
 const searchArticles = (request, response, next) => {
   // only searches a single source, rather than an
   // array of sources
-  // also doesn't allow sortBy filtering yet, as some
-  // sources don't support all types of filtering
-  const { source } = request.query;
+  const { source, sortBy } = request.query;
+  console.log(sortBy);
 
   const search = `https://newsapi.org/v1/articles?apiKey=${process.env.NEWS_KEY}&source=${source}`;
 
-  axios.get(search)
-    .then((newsResponse) => {
-      const articles = newsResponse.data.articles.map((article) => {
-        const newArticle = article;
-        newArticle.source = source;
-        return newArticle;
-      });
-      request.articles = articles;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-      response.status(500);
-      response.end();
+  getArticles(search, sortBy, (newsData) => {
+    const articles = newsData.articles.map((article) => {
+      const newArticle = article;
+      newArticle.source = source;
+      return newArticle;
     });
+    request.articles = articles;
+    next();
+  }, (err) => {
+    console.log(err);
+    response.status(500).send('Error--request can\'t be processed.');
+  });
 };
 
 export default searchArticles;
