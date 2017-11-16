6eba9814889b993f6010f2d7f968f1e94d61335b
Danny Welstad <dannywelstad@gmail.com>
2017-11-15 17:13:32 -0800

    (feat) add database helper functions for interacting with database - needs work

--- app/database/db.js
+++ app/database/db.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import config from "../config/config";
-import User from "./models/source";
+import mongoose from 'mongoose';
+import config from '../config/config';
+import Source from './models/source';
 
 const uri = `mongodb://${config.username}:${config.password}@ds163705.mlab.com:63705/news-stand`;
 
@@ -8,10 +8,10 @@ mongoose.connect(uri);
 
 const db = mongoose.connection;
 
-db.on("error", console.log("connection error:"));
+db.on('error', console.log('connection error:'));
 
-db.once("open", () => {
-  console.log("connection to mongoDb open");
+db.once('open', () => {
+  console.log('connection to mongoDb open');
 });
 
 export default db;
