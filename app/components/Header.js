4727aeb01b2ae93d6952b5f9f66a75b0c599e7da
Danny Welstad <dannywelstad@gmail.com>
2017-11-18 13:39:29 -0800

    get newsAPI working for default request

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = (props) => (
+const Header = props => (
   <div className="header">
     <form>
       [News Stand Logo]
--- app/components/NewsItem.js
+++ app/components/NewsItem.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-const NewsItem = ({article}) => {
-  return (
-    <div className="newsItem">
-      <img src={article.urlToImage} className='articleImg' />>
-      <h3 className='articleTitle'>{article.title}</h3>
-      <p className='articleDescription'>{article.description}</p>
-      {/* <p className='articleSource'>{article.source}</p> */}
-      <p className='articleAuthor'>{article.author}</p>
-      <p className='articleUrl'>{article.url}</p>
-      <br/>
-    </div>
-  )
-}
+const NewsItem = ({ article }) => (
+  <div className="newsItem">
+    <img src={article.urlToImage} className="articleImg" alt="#" />
+    <h3 className="articleTitle">{article.title}</h3>
+    <p className="articleDescription">{article.description}</p>
+    {/* <p className='articleSource'>{article.source}</p> */}
+    <p className="articleAuthor">{article.author}</p>
+    <p className="articleUrl">{article.url}</p>
+    <br />
+  </div>
+);
+
 
 export default NewsItem;
 // TODO: uncomment source when we have backend-crafted source data
