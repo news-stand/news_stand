d0987af758b7f86c14478e6b04ff4e629bbc28cf
Danny Welstad <dannywelstad@gmail.com>
2017-11-18 12:12:54 -0800

    set up first node test file

--- test/server/serverSpec.js
+++ test/server/serverSpec.js
@@ -1,8 +1,9 @@
 const request = require('request');
 
-const baseUrl = 'http://localhost:8080/';
+const baseUrl = 'http://localhost:8080';
 
 describe("News Stand Server", function() {
+
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(baseUrl, function(error, response, body) {
@@ -11,4 +12,32 @@ describe("News Stand Server", function() {
       });
     });
   });
+
+  //I beleive this test is failing because the middlware function is throwing an error
+  // I don't have a .env file setup so I don't think I am actually able to make a request
+  describe("GET /articles", function() {
+    it("returns status code 200", function(done) {
+      request.get(`${baseUrl}/articles`, function(error, response, body) {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+    });
+
+    //not totally sure if this is how it works
+    it("returns an array", function(done) {
+      request.get(`${baseUrl}/articles`, function(error, response, body) {
+        expect(response.data).toBe(200);
+        done();
+      });
+    });
+
+    it("returns an array of objects", function(done) {
+      request.get(`${baseUrl}/articles`, function(error, response, body) {
+        expect(response.data).toBe(200);
+        done();
+      });
+    });
+
+  });
+
 });
