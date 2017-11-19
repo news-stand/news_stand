a5f47b6876cb310ea4428fa19c8f5e69a296f4c1
Katherine Manning <katm201@gmail.com>
2017-11-19 10:05:25 -0800

    (feat) add state to search component

--- app/components/TopicsSearch.js
+++ app/components/TopicsSearch.js
@@ -3,12 +3,19 @@ import React from 'react';
 class TopicsSearch extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchTerm: '',
+    };
   }
   render() {
     return (
       <div className="topics search">
         <form className="topics search">
-          <input className="topics search" placeholder="Search a Topic" />
+          <input
+            className="topics search"
+            placeholder="Search a Topic"
+            value={this.state.searchTerm}
+          />
           <button type="button" >Search</button>
         </form>
       </div>
