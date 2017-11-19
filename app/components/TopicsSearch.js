0eb317d41d006900f1228bfae707460dd990bf3f
Katherine Manning <katm201@gmail.com>
2017-11-19 10:08:49 -0800

    (feat) add method to allow search bar to reflect what's been typed

--- app/components/TopicsSearch.js
+++ app/components/TopicsSearch.js
@@ -6,7 +6,15 @@ class TopicsSearch extends React.Component {
     this.state = {
       searchTerm: '',
     };
+
+    this.handleSearchChange = this.handleSearchChange.bind(this);
   }
+
+  handleSearchChange(event) {
+    const { value } = event.target;
+    this.setState({ searchTerm: value });
+  }
+
   render() {
     return (
       <div className="topics search">
@@ -15,6 +23,7 @@ class TopicsSearch extends React.Component {
             className="topics search"
             placeholder="Search a Topic"
             value={this.state.searchTerm}
+            onChange={(event) => { this.handleSearchChange(event); }}
           />
           <button type="button" >Search</button>
         </form>
