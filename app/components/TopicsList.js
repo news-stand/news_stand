5fa4e394c8e78354f625594739fcf38bffbcb2c1
Katherine Manning <katm201@gmail.com>
2017-11-19 09:54:52 -0800

    (feat) dynamically render topics list items from topics

--- app/components/TopicsList.js
+++ app/components/TopicsList.js
@@ -4,7 +4,6 @@ import TopicsListItem from './TopicsListItem';
 const TopicsList = (props) => (
   <div className="topics list">
     <h5>TopicsList</h5>
-    <TopicsListItem className="topics list-item" />
     {props.topics && props.topics.map((topic, key) => {
       return <TopicsListItem className="topics list-item" topic={topic} key={key} />;
     })}
