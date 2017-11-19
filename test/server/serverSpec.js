3e9a3f9bca9d6a32f291ff34dfec51b9aa13beaf
Katherine Manning <katm201@gmail.com>
2017-11-18 16:37:19 -0800

    (refactor) update tests to use single quotes, not double quotes

--- test/server/serverSpec.js
+++ test/server/serverSpec.js
@@ -2,10 +2,10 @@ const request = require('request');
 
 const baseUrl = 'http://localhost:8080';
 
-describe("News Stand Server", function() {
+describe('News Stand Server', function() {
 
-  describe("GET /", function() {
-    it("returns status code 200", function(done) {
+  describe('GET /', function() {
+    it('returns status code 200', function(done) {
       request.get(baseUrl, function(error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
@@ -15,8 +15,8 @@ describe("News Stand Server", function() {
 
   //I beleive this test is failing because the middlware function is throwing an error
   // I don't have a .env file setup so I don't think I am actually able to make a request
-  describe("GET /articles", function() {
-    it("returns status code 200", function(done) {
+  describe('GET /articles', function() {
+    it('returns status code 200', function(done) {
       request.get(`${baseUrl}/articles`, function(error, response, body) {
         expect(response.statusCode).toBe(200);
         done();
@@ -24,14 +24,14 @@ describe("News Stand Server", function() {
     });
 
     //not totally sure if this is how it works
-    it("returns an array", function(done) {
+    it('returns an array', function(done) {
       request.get(`${baseUrl}/articles`, function(error, response, body) {
         expect(response.data).toBe(200);
         done();
       });
     });
 
-    it("returns an array of objects", function(done) {
+    it('returns an array of objects', function(done) {
       request.get(`${baseUrl}/articles`, function(error, response, body) {
         expect(response.data).toBe(200);
         done();
