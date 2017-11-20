677ddf2ef8f252120c630782caa9f8aa7413e401
Danny Welstad <dannywelstad@gmail.com>
2017-11-20 11:26:47 -0800

    (feat) add react-router-dom to node_moudules and add tests to SourceItem

--- test/client/SourceItemSpec.js
+++ test/client/SourceItemSpec.js
@@ -2,10 +2,23 @@ import React from 'react';
 import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import AddSource from '../../app/components/SourceItem';
+import SourceItem from '../../app/components/SourceItem';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<SourceItem />', function () {
+  let wrapper, source;
 
+  beforeEach(function () {
+    wrapper = shallow(<SourceItem source={'CNN'} />);
+    source = 'CNN';
+  });
+
+  it('renders passed in source', () => {
+    expect(wrapper).find('p').text().toEqual('CNN');
+  })
+
+  it('has a div with className selectedSources', () => {
+    expect(wrapper).find('.selectedSources').length.toEqual(1);
+  })
 });
