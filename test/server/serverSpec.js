51babe0696e2badd4bba513fdd35393fbaa1957f
Katherine Manning <katm201@gmail.com>
2017-11-19 16:06:08 -0800

    (test) add tests for the topics/sources search

--- test/server/serverSpec.js
+++ test/server/serverSpec.js
@@ -17,8 +17,6 @@ describe('News Stand Server', function() {
     });
   });
 
-  // I beleive this test is failing because the middlware function is throwing an error
-  // I don't have a .env file setup so I don't think I am actually able to make a request
   describe('GET /articles for top headlines', function() {
     const options = {
       params: {
@@ -26,6 +24,52 @@ describe('News Stand Server', function() {
         source: null,
         sortBy: null,
         topHeadlines: true,
+      },
+    };
+
+    it('returns status code 200', function(done) {
+      axios.get(`${baseUrl}/articles`, options)
+        .then((response) =>{
+          expect(response.status).toBe(200);
+          done();
+        })
+        .catch((err) => {
+          throw new Error('Error with GET to route /articles ', err);
+        });
+    });
+
+    //not totally sure if this is how it works
+    it('returns an array', function(done) {
+      axios.get(`${baseUrl}/articles`, options)
+        .then((response) =>{
+          expect(Array.isArray(response.data)).toBe(true);
+          done();
+        })
+        .catch((err) => {
+          throw new Error('Error with GET to route /articles ', err);
+        });
+    });
+
+    it('returns an array of objects', function(done) {
+      axios.get(`${baseUrl}/articles`, options)
+        .then((response) =>{
+          expect(typeof response.data[0]).toBe('object');
+          done();
+        })
+        .catch((err) => {
+          throw new Error('Error with GET to route /articles ', err);
+        });
+    });
+
+  });
+
+  describe('GET /articles for topics and sources search', function() {
+    const options = {
+      params: {
+        topic: ['art', 'music'],
+        source: ['bbc-news', 'cnn'],
+        sortBy: 'popularity',
+        topHeadlines: false,
       },
     };
 
