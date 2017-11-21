b4b58936b8a58438e8f30983fc24fb17f05f93fe
Katherine Manning <katm201@gmail.com>
2017-11-21 13:24:41 -0800

    (refactor) update styling of topics and sources items to be centered under their input fields

--- app/components/TopicsList.js
+++ app/components/TopicsList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import TopicsListItem from './TopicsListItem';
 
 const TopicsList = props => (
-  <div className="topics list">
-    <div className="topics list">
+  <div className="topics-list-container">
+    <div className="topics-list">
       {props.topics && props.topics.map((topic, key) => (
         <TopicsListItem
           className="topics list-item"
