149bb7426eb0a93aa234ce43e7a04ecd16c00cae
Katherine Manning <katm201@gmail.com>
2017-11-21 10:31:49 -0800

    (fix) add a check for props.newsArticles before attempting to map over them

--- app/components/NewsList.js
+++ app/components/NewsList.js
@@ -3,7 +3,7 @@ import NewsItem from './NewsItem';
 
 const NewsList = props => (
   <div>
-    {props.newsArticles.map((article) => {
+    {props.newsArticles && props.newsArticles.map((article) => {
       return <NewsItem article={article} key={article.url} />;
     })}
   </div>
