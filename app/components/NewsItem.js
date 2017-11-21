d9924c076aebf4bb87287764cf5bef27744a5b4e
Katherine Manning <katm201@gmail.com>
2017-11-21 10:40:42 -0800

    (fix) update to double quotes around className to fix linting error

--- app/components/NewsItem.js
+++ app/components/NewsItem.js
@@ -5,7 +5,7 @@ const NewsItem = ({ article }) => (
     <img src={article.urlToImage} className="articleImg" alt="#" />
     <h3 className="articleTitle">{article.title}</h3>
     <p className="articleDescription">{article.description}</p>
-    <p className='articleSource'>{article.source.name}</p>
+    <p className="articleSource">{article.source.name}</p>
     <p className="articleAuthor">{article.author}</p>
     <p className="articleUrl">{article.url}</p>
     <br />
