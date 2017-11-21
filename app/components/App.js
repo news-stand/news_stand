37e1f2496549400c52d199a1c65f664ddddb15c0
Patrick Miner <patrickminer318@gmail.com>
2017-11-20 17:15:17 -0800

    Fixing bugs after the rebase

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,9 @@
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
-// import axios from 'axios';
 
-import NotFound from './NotFound';
-import Login from './Login';
 import Home from './Home';
+import Login from './Login';
+import NotFound from './NotFound';
 
 
 class App extends React.Component {
@@ -14,82 +13,24 @@ class App extends React.Component {
     this.state = {};
   }
 
-
   render() {
     return (
-<<<<<<< HEAD
-      <div>
-        <Header
-          onRefreshClick={this.onRefreshClick}
-          onToggleClick={this.onToggleClick}
-          mostPopular={this.state.mostPopular}
-        />
-        {/* Space savers for the eventual 'login' and signup links we'll want to set up */}
-        {/* <div id="accounts">
-          <div><Link to="/signup"></Link></div>
-          <div><Link to="/login">Login</Link></div>
-        </div> */}
-
-        <hr />
+      <Router>
         <Switch>
           <Route
             exact
             path="/"
-            component={() =>
-              ( // Need to refactor this to be it's own 'Home' component
-                <div>
-                  <Topics
-                    className="topics"
-                    topics={this.state.topics}
-                    onTopicSearch={this.onTopicSearch}
-                    onTopicRemoval={this.onTopicRemoval}
-                  />
-                  <AddSource onAddSource={this.onAddSource} />
-                  <SelectedSources selectedSources={this.state.selectedSources} />
-                  <NewsList newsArticles={this.state.articles} />
-                </div>
-              )
-            }
-
-=======
-      <Router>
-        <div>
-<<<<<<< HEAD
-          <Header
-            onRefreshClick={this.onRefreshClick}
-            onToggleClick={this.onToggleClick}
-            mostPopular={this.state.mostPopular}
+            component={Home}
           />
-          <Topics
-            className="topics"
-            topics={this.state.topics}
-            onTopicSearch={this.onTopicSearch}
-            onTopicRemoval={this.onTopicRemoval}
->>>>>>> (bug fix) fixed '<Switch> outside of <Router> bug that was failing tests
+          <Route
+            path="/login"
+            component={Login}
           />
-          {/* Space savers for the eventual 'login' and signup links we'll want to set up */}
-          {/* <div id="accounts">
-            <div><Link to="/signup"></Link></div>
-            <div><Link to="/login">Login</Link></div>
-          </div> */}
-
-          <hr />
-=======
->>>>>>> Moved all code from App.js to Home.js.  Next step is to figure out how to render components server-side
-          <Switch>
-            <Route
-              exact
-              path="/"
-              component={Home}
-            />
-            <Route
-              path="/login"
-              component={Login}
-            />
-            {/* <Route path="/signup" component={Signup} /> */}
-            <Route component={NotFound} />
-          </Switch>
-        </div>
+          <Route
+            component={NotFound}
+          />
+          {/* Will want to add signup component */}
+        </Switch>
       </Router>
     );
   }
