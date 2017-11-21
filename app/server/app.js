963b21207a656618f57c4ab0647f2de0555c2002
Danny Welstad <dannywelstad@gmail.com>
2017-11-20 17:05:49 -0800

    (feat) google Oauth

--- app/server/app.js
+++ app/server/app.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import express from 'express';
 import searchArticles from './middleware/bySource';
+import authRoutes from './auth-routes';
+import passportSetup from '../../config/passport-setup';
 
 const app = express();
 
@@ -8,6 +10,7 @@ const publicPath = express.static(path.join(__dirname, '../'));
 const indexPath = path.join(__dirname, '../index.html');
 
 app.use(publicPath);
+app.use('/auth', authRoutes);
 
 app.get('/articles', searchArticles, (request, response) => {
   const { articles } = request;
