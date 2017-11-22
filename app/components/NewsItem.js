828b645f0351f10ccd915ab50a901f35d14f8ea8
Patrick Miner <patrickminer318@gmail.com>
2017-11-21 23:36:02 -0800

    (feat) made it so when news article elements aren't provided they do not render

--- app/components/NewsItem.js
+++ app/components/NewsItem.js
@@ -2,12 +2,56 @@ import React from 'react';
 
 const NewsItem = ({ article }) => (
   <div className="newsItem">
-    <img src={article.urlToImage} className="articleImg" alt="#" />
-    <h3 className="articleTitle">{article.title}</h3>
-    <p className="articleDescription">{article.description}</p>
-    <p className="articleSource">{article.source.name}</p>
-    <p className="articleAuthor">{article.author}</p>
-    <p className="articleUrl">{article.url}</p>
+    {
+      article.urlToImage ?
+        <img
+          src={article.urlToImage}
+          className="articleImg"
+          alt="#"
+        /> :
+        <div
+          className="hidden"
+        />
+    }  
+    {/* <img src={article.urlToImage} className="articleImg" alt="#" /> */}
+
+    {
+      article.title ?
+        <h3 className="articleTitle">
+          {article.title}
+        </h3> :
+        <h3 className="hidden">ARTICLE MISSING TITLE</h3>
+    }
+    {/* <h3 className="articleTitle">{article.title}</h3> */}
+
+    {
+      article.description ?
+        <p className="articleDescription">{article.description}</p> :
+        <p className="hidden">ARTICLE MISSING DESCRIPTION</p>
+    }
+    {/* <p className="articleDescription">{article.description}</p> */}
+
+    {
+      article.source.name ?
+        <p className="articleSource">{article.source.name}</p> :
+        <p className="hidden">ARTICLE MISSING SOURCE NAME</p>
+    }
+    {/* <p className="articleSource">{article.source.name}</p> */}
+
+    {
+      article.author ?
+        <p className="articleAuthor">{article.author}</p> :
+        <p className="hidden">ARTICLE MISSING AUTHOR</p>
+    }
+    {/* <p className="articleAuthor">{article.author}</p> */}
+
+    {
+      article.url ?
+        <p className="articleUrl">{article.url}</p> :
+        <p className="hidden">ARTICLE MISSING URL</p>
+    }
+    {/* <p className="articleUrl">{article.url}</p> */}
+
     <br />
   </div>
 );
