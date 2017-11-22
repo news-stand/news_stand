14d5941a9fcd856ec7a2493fad509adb121504ce
Danny Welstad <dannywelstad@gmail.com>
2017-11-22 13:49:10 -0800

    got rid of keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  google: {
-    clientID: '955308957183-bvapoiummgn2slsgp8mpbca5b3aaakdn.apps.googleusercontent.com',
-    clientSecret: 'qmRQzgH76aA0cXjQ--yWb-Pq',
-  },
-  mongodb: {
-    URI: 'mongodb://danny:danny@ds163705.mlab.com:63705/news-stand',
-  },
-};
