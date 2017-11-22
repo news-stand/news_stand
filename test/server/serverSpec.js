77792283d69dee9b7b0ad5a886b1acd3bcefabb2
Katherine Manning <katm201@gmail.com>
2017-11-22 12:53:50 -0800

    (test) add tests for empty topics and sources searches

--- test/server/serverSpec.js
+++ test/server/serverSpec.js
@@ -110,10 +110,65 @@ describe('News Stand Server', function() {
 
   });
 
-  describe('GET /articles for multiple topics and, but no sources', function() {
+  describe('GET /articles for multiple topics, but no sources', function() {
     const options = {
       params: {
         topic: ['art', 'music'],
+        selectedSources: [],
+        sortBy: 'popularity',
+      },
+    };
+
+    it('returns status code 200', function(done) {
+      axios.get(`${baseUrl}/articles`, options)
+        .then((response) =>{
+          expect(response.status).toBe(200);
+          done();
+        })
+        .catch((err) => {
+          throw new Error('Error with GET to route /articles ', err);
+        });
+    });
+
+    it('returns an array', function(done) {
+      axios.get(`${baseUrl}/articles`, options)
+        .then((response) =>{
+          expect(response.data).toBeArray();
+          done();
+        })
+        .catch((err) => {
+          throw new Error('Error with GET to route /articles ', err);
+        });
+    });
+
+    it('returns articles with a default source of espn', function(done) {
+      axios.get(`${baseUrl}/articles`, options)
+        .then((response) =>{
+          expect(response.data[0].source.name.toLowerCase()).toBe('espn');
+          done();
+        })
+        .catch((err) => {
+          throw new Error('Error with GET to route /articles ', err);
+        });
+    });
+
+    it('returns an array of objects', function(done) {
+      axios.get(`${baseUrl}/articles`, options)
+        .then((response) =>{
+          expect(response.data).toBeArrayOfObjects();
+          done();
+        })
+        .catch((err) => {
+          throw new Error('Error with GET to route /articles ', err);
+        });
+    });
+
+  });
+
+  describe('GET /articles for empty topics and sources', function() {
+    const options = {
+      params: {
+        topic: [],
         selectedSources: [],
         sortBy: 'popularity',
       },
