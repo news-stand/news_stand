097b36411a71d962b0fac541a29caf2a0fdaa946
Katherine Manning <katm201@gmail.com>
2017-11-24 13:59:18 -0800

    (fix) update TopicsListSpec to remove prop validation warnings

--- test/client/topicsListSpec.js
+++ test/client/topicsListSpec.js
@@ -9,21 +9,26 @@ import TopicsListItem from '../../app/components/TopicsListItem';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-xdescribe('<TopicsList />', function () {
+describe('<TopicsList />', function () {
+  const dummyFn = () => {
+    console.log('dummy function');
+  };
+
   it('contains a <TopicsListItem /> component', function() {
     const topics = ['politics'];
-    const wrapper = shallow(<TopicsList topics={topics} />);
+    const wrapper = shallow(<TopicsList topics={topics} onRemoval={dummyFn} />);
     expect(wrapper.find(TopicsListItem).length).toEqual(1);
   });
 
   it('doesn\'t render <TopicsListItem /> component if no topics are passed in', function() {
-    const wrapper = shallow(<TopicsList />);
+    const topics = [];
+    const wrapper = shallow(<TopicsList topics={topics} onRemoval={dummyFn} />);
     expect(wrapper.find(TopicsListItem).length).toEqual(0);
   });
 
   it('contains a <TopicsListItem /> component that dynamically renders topics', function() {
     const topics = ['politics', 'art'];
-    const wrapper = shallow(<TopicsList topics={topics} />);
+    const wrapper = shallow(<TopicsList topics={topics} onRemoval={dummyFn} />);
     expect(wrapper.find(TopicsListItem).length).toEqual(2);
   });
 });
