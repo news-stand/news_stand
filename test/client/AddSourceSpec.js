04779c4eac9ffe4b72c76c7c9b1431240ab6cda4
Katherine Manning <katm201@gmail.com>
2017-11-24 14:31:11 -0800

    (fix) update AddSourceSpec to remove prop types warnings

--- test/client/AddSourceSpec.js
+++ test/client/AddSourceSpec.js
@@ -9,29 +9,35 @@ import Autosuggest from "react-autosuggest";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-xdescribe('<AddSource />', function () {
+describe('<AddSource />', function () {
+  const dummyFn = () => {
+    console.log('dummy function');
+  };
+
+  const addSource = (<AddSource onAddSource={dummyFn} />);
+
   it('should have handleSuggestionsFetchRequested function defined', function () {
-    const wrapper = shallow(<AddSource />);
+    const wrapper = shallow(addSource);
     expect(wrapper.props().handleSuggestionsFetchRequested).toBe.defined;
   });
 
   it('should have handleSuggestionsClearRequested function defined', function () {
-    const wrapper = shallow(<AddSource />);
+    const wrapper = shallow(addSource);
     expect(wrapper.props().handleSuggestionsClearRequested).toBe.defined;
   });
 
   it('should have handleChange function defined', function () {
-    const wrapper = shallow(<AddSource />);
+    const wrapper = shallow(addSource);
     expect(wrapper.props().handleChange).toBe.defined;
   });
 
   it('contains an <Autosuggest /> component', function () {
-    const wrapper = mount(<AddSource />);
+    const wrapper = mount(addSource);
     expect(wrapper.find(Autosuggest).length).toEqual(1);
   });
 
   it('contains a div with class addSourceContainer', function () {
-    const wrapper = mount(<AddSource />);
+    const wrapper = mount(addSource);
     expect(wrapper.find('.addSourceContainer').length).toEqual(1);
   });
 });
