7f6d89d4c67b1d551efbbf1a2a8792668d403260
Danny Welstad <dannywelstad@gmail.com>
2017-11-24 08:58:30 -0800

    (feat) functional login and logout

--- app/components/Login.js
+++ app/components/Login.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Login = () => (
-  <div> This is your login page</div>
+  <div>
+    <div> This is your login page</div>
+    <a href="/auth/google"><button>Login With Google</button></a>
+  </div>
 );
 
 export default Login;
