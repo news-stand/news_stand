e55e9598690b553f1064fefe09741fced45af27a
Ayush Kumar <ayushkumar26@gmail.com>
2017-11-24 14:04:07 -0800

    remove logo file, insert directly into header component

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import logo from '../public/assets/logo';
 
 const Header = props => (
   <div className="header">
     <form>
-      <img src={logo} alt="news stand" className="logo" />
+      <img src="https://i.imgur.com/mCSoavu.png" alt="news stand" className="logo" />
       Viewing {props.sortBy === 'popularity' ? 'most popular' : 'most recent'} news.
       <button type="button" className="btn btn-primary" onClick={props.onToggleClick}>{props.sortBy === 'popularity' ? 'View most recent' : 'View trending'} </button>
       <button
--- app/public/assets/logo.js
+++
@@ -1,1 +0,0 @@
-module.exports = 'https://i.imgur.com/mCSoavu.png';
