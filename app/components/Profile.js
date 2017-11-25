90a0c72cdaf3b971ff33eacafbd705a758aa4e43
Patrick Miner <patrickminer318@gmail.com>
2017-11-25 12:28:01 -0800

    (feat) added Topics and Sources lists to user profile page

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Topics from './Topics';
 import TopicsList from './TopicsList';
 import SelectedSources from './SelectedSources';
 import NewsList from './NewsList';
 import NewsItem from './NewsItem';
 
-const Profile = ({user}) => (
+const Profile = ({ user }) => (
   <div id="profile">
     <div className="user">
       <h2>{user.username}</h2>
     </div>
+
+    {/* Favorite topics list */}
+    <div className="profileTopicsList">
+      {user.topics.map(topicString => <p>{topicString}</p>)}
+    </div>
+
+    {/* Selected Sources List */}
+    <div className="profileSourcesList">
+      {user.selectedSources.map(sourceObj => <p>{sourceObj.label}</p>)}
+    </div>
+
     
   </div>
-)
+);
 
 Profile.propTypes = {
   user: PropTypes.shape({
