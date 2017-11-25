ba8fb2581738403916bc78c8fcee7273e303657d
Katherine Manning <katm201@gmail.com>
2017-11-25 11:53:09 -0800

    (feat) add conditional to addFavorite

--- app/server/middleware/addFavorite.js
+++ app/server/middleware/addFavorite.js
@@ -1,7 +1,22 @@
 import User from '../database/models/user';
 
 const addFavorite = (request, response, next) => {
-  console.log('USER': request.user);
+  console.log('USER: ', request.user);
+
+  if (request.user) {
+    // User.findOneAndUpdate(
+    //   { googleId: request.user.googleId },
+    //   { $set: { topics: request.body.topics } },
+    //   (err, doc) => {
+    //     if (err) {
+    //       console.log('There was an error updating preferences: ', err);
+    //     } else {
+    //       console.log('Successfully updated doc: ', doc);
+    //     }
+    //   },
+    // );
+  }
+
   next();
 };
 
