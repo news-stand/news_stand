ac7a70b2dd4e967e3c1233c2d04fe995216a6b83
Katherine Manning <katm201@gmail.com>
2017-11-26 10:34:18 -0800

    (fix) remove duplicates from NewsList

--- app/components/NewsList.js
+++ app/components/NewsList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import uniq from 'node-uniq';
+
 import NewsItem from './NewsItem';
 
 const NewsList = props => (
   <div>
-    {props.newsArticles && props.newsArticles.map(article => (
-      <NewsItem article={article} key={article.url} />
-    ))}
+    {props.newsArticles && uniq(props.newsArticles, i => i.url)
+        .map(article => (
+          <NewsItem article={article} key={article.url} />
+        ))
+    }
   </div>
 );
 
