4947b31cfc2cbc327df509d614003210cbe9e6a2
Danny Welstad <dannywelstad@gmail.com>
2017-11-26 11:40:55 -0800

    Oauth commetns

--- app/server/auth-routes.js
+++ app/server/auth-routes.js
@@ -1,13 +1,19 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+
+// route to check if a user is logged in
+// If a user is logged in, Passport creates a user object on the request object.
+// the user object come from the 'users' collection in the database (i.e. they are the same)
 router.get('/', (request, response) => {
   if (request.user) {
+    // is user is logged in
     response.json({
       loggedIn: true,
       user: request.user,
     });
   } else {
+    // is user is not logged in
     response.json({
       loggedIn: false,
       user: {},
@@ -15,16 +21,30 @@ router.get('/', (request, response) => {
   }
 });
 
+// ----- OAUTH STEP 1: Passport Middleware ----- //
+// passport middleware redirects the user to google sign in
 router.get('/google', passport.authenticate('google', {
+  // scope specifies what information we are requesting from the users Google profile
   scope: ['profile'],
 }));
 
-router.get('/logout', (request, response) => {
-  request.logout();
+// ----- OAUTH STEP 2: Passport Middleware ----- //
+// After the user logs in, google redirects back to this route as defined in passport-setup.js
+// Google sends back a token in the url that can be used to get the users data.
+
+// NOTE: user profile info is not sent back after the user signs in, Passport must make a subsequent
+// request with previosuly provided token to get info. Passport does this for us behind the scenes.
+
+// on resonse, the callback function in passport-setup.js fires
+router.get('/google/redirect', passport.authenticate('google', { failureRedirect: '/' }), (request, response) => {
+  // ----- OAUTH STEP 5 ----- //
+  // on success OR failure to sign in, user is redirected to home page
+  // if successfull, user now has a valid cookie
   response.redirect('/');
 });
 
-router.get('/google/redirect', passport.authenticate('google', { failureRedirect: '/' }), (request, response) => {
+router.get('/logout', (request, response) => {
+  request.logout();
   response.redirect('/');
 });
 
--- app/server/config/passport-setup.js
+++ app/server/config/passport-setup.js
@@ -5,26 +5,41 @@ import User from '../database/models/user';
 
 dotenv.config();
 
+// ----- OAUTH STEP 4: More Passport Middleware ----- //
 passport.serializeUser((user, done) => {
+  // use id from database to make a session and put it in the cookie
   done(null, user.id);
 });
 
+// ----- OAUTH: Check to see if user has a valid session ----- //
 passport.deserializeUser((id, done) => {
+  // decode cookie token and check to see if valid
   User.findById(id).then((user) => {
     done(null, user);
   });
 });
 
+
+// Setup Google Strategy.
+// clientId and clientSecret come from singing up for Oauth through the Google Developer Console
+// https://console.developers.google.com/apis
+// callbck URL specifies where google shoudl send the user after they sign in
+// this route is handles in auth-routes.js
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   callbackURL: '/auth/google/redirect',
 }, (accessToken, refreshToken, profile, done) => {
+  // ----- OAUTH STEP 3: More Passport Middleware ----- //
+  // profile has been sent back in exchange for token
   User.findOne({ googleId: profile.id })
+  //  check if user is already in database
     .then((currentUser) => {
       if (currentUser) {
+        // if they are, send send porfile to next middleware (Step: 4)
         done(null, currentUser);
       } else {
+        // if they aren't, create new user and send to next middleware (Step: 4)
         new User({
           username: profile.displayName,
           googleId: profile.id,
