cb56e7555ba4883eaba2ba4388f72d6ca91801e6
Jason Borer <borerjason@gmail.com>
2017-11-29 21:18:11 -0800

    add article to index.js

--- app/reducers/articles-reducer.js
+++ app/reducers/articles-reducer.js
@@ -3,7 +3,7 @@ import { ADD_ARTICLE } from '../actions/index';
 export default function (state = [], action) {
   switch (action.type) {
     case ADD_ARTICLE:
-      return [action].concat(state);
+      return [action.payload].concat(state);
     default:
       return state;
   }
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import UserReducer from './user-reducer';
+import ArticleReducer from './articles-reducer';
 
 const rootReducer = combineReducers({
   user: UserReducer,
+  articles: ArticleReducer,
 });
 
 export default rootReducer;
